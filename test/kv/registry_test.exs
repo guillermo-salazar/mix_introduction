315282f9021cbd84167bdb839d5c0c5c31c82726
Guillermo Salazar <guillermos_2007@yahoo.com>
2017-11-26 12:21:22 -0500

    Added test for Registry

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -6,5 +6,13 @@ defmodule KV.RegistryTest do
     %{registry: registry}
   end
 
-  
+  test "spawns buckets", %{ registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
+
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    KV.Bucket.put(bucket, "milk", 1)
+    assert KV.Bucket.get(bucket, "milk") == 1
+  end
 end
